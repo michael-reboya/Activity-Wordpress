aca675ec9cbdc0099daaa43ce43bb97acf7cc770
michael-reboya <michaeljohnreboya@gmail.com>
2021-04-14 08:51:45 +0800

    Update About Temp

--- wp-content/themes/blank/template/about-template.php
+++ wp-content/themes/blank/template/about-template.php
@@ -11,6 +11,6 @@
 <div class="container classnisir">
 	<h1>Its all about shits</h1>
 	<h3>dont panic, its oraganic</h3>
-	<p>shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit </p>
+	<p></p>
 </div>
  <?php get_footer(); ?>
